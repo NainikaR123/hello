1f88720ed94db72879f959005c907887916d6a6b
NainikaR123 <nainikagowda669@gmail.com>
2025-03-25 11:54:27 +0530

    Update hello.c
    The hello world program is updated to addition.

--- hello.c
+++ hello.c
@@ -1,5 +1,25 @@
 #include<stdio.h>
+int add(int n1, int n2)
+{
+    return n1 - n2;
+}
+
 int main()
 {
+    int n1, n2, result;
+
+    printf("Enter the first number: ");
+    scanf("%d", &n1);
+
+    printf("Enter the second number: ");
+    scanf("%d", &n2);
+
+    result = add(n1, n2);
+
+    printf("The addition is: %d\n", result);
+
+    return 0;
+}
+
 printf("Hello world\n");
 }
